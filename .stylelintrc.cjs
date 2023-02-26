6e4ed1fb2a4b0905d96abe5d55afb8c485fc9af7
STAS-X <rosteh82@gmail.com>
2023-02-26 16:39:18 +0300

    update lints options

--- .stylelintrc.cjs
+++ .stylelintrc.cjs
@@ -1,9 +1,8 @@
 module.exports = {
   extends: [
     'stylelint-config-standard-scss',
-    'stylelint-config-styled-components',
   ],
-  plugins: ['stylelint-order', 'stylelint-scss'],
+  plugins: ['stylelint-scss'],
   rules: {
     indentation: [4],
     'selector-class-pattern': null,
