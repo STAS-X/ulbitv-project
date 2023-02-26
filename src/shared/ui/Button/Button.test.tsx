9c1faec872ad7df34549998c20de25ac70675c2d
STAS-X <rosteh82@gmail.com>
2023-02-26 18:42:07 +0300

    fix jest test

--- src/shared/ui/Button/Button.test.tsx
+++ src/shared/ui/Button/Button.test.tsx
@@ -1,10 +1,24 @@
 import { render, screen } from '@testing-library/react';
+import componentWithProvider from 'shared/lib/renderTest/componentWithProvider';
 import { Button } from 'shared/ui/Button/Button';
 
 describe('button test', () => {
 	const text = 'Test';
 
+	let container: HTMLDivElement;
+
+	beforeEach(() => {
+		container = document.createElement('div');
+		document.body.appendChild(container);
+	});
+
+	afterEach(() => {
+		document.body.removeChild(container);
+		container = null;
+	});
+
 	test('first component test', () => {
+		// Test first render and componentDidMount
 		render(<Button>{text}</Button>);
 		expect(screen.getByText('Test')).toBeInTheDocument();
 	});
--- src/widgets/Sidebar/ui/Sidebar.test.tsx
+++ src/widgets/Sidebar/ui/Sidebar.test.tsx
@@ -1,17 +1,22 @@
 import { fireEvent, screen } from '@testing-library/react';
 import { Sidebar } from 'widgets/Sidebar';
 import componentWithProvider from 'shared/lib/renderTest/componentWithProvider';
+import { act } from 'react-dom/test-utils';
 
 describe('button test', () => {
-	test('Sidebar test with provider HOC', () => {
+	test('Sidebar test with provider HOC', async () => {
 		// const SideBarHoc = withTranslation()(Sidebar);
-		componentWithProvider(<Sidebar />);
+		await act(async () => {
+			componentWithProvider(<Sidebar />);
+		});
 		expect(screen.getByTestId('sidebar')).toBeInTheDocument();
 	});
 
-	test('Sidebar test collapse', () => {
+	test('Sidebar test collapse', async () => {
 		// const SideBarHoc = withTranslation()(Sidebar);
-		componentWithProvider(<Sidebar />);
+		await act(async () => {
+			componentWithProvider(<Sidebar />);
+		});
 		const toggleBtn = screen.getByTestId('sidebar-toggle');
 		expect(screen.getByTestId('sidebar')).toBeInTheDocument();
 
