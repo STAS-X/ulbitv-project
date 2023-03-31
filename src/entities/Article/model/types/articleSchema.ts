a1ab3737e86f8c179077e03d32fb80a2157a4b96
STAS-X <rosteh82@gmail.com>
2023-03-31 16:42:48 +0300

    add article types

---
+++ src/entities/Article/model/types/articleSchema.ts
@@ -0,0 +1,44 @@
+export interface ArticleBaseCodeBlock {
+	id: string;
+	type: ArticleBlockType;
+}
+
+export interface ArticleCodeBlock extends ArticleBaseCodeBlock {
+	type: ArticleBlockType.CODE;
+	code: string;
+}
+export interface ArticleImageBlock extends ArticleBaseCodeBlock {
+	type: ArticleBlockType.IMAGE;
+	src: string;
+	title: string;
+}
+export interface ArticleTextBlock extends ArticleBaseCodeBlock {
+	type: ArticleBlockType.TEXT;
+	title?: string;
+	paragraphs: Array<string>;
+}
+
+export type ArticleBlock = ArticleCodeBlock | ArticleImageBlock | ArticleTextBlock;
+
+export enum ArticleBlockType {
+	CODE = 'CODE',
+	IMAGE = 'IMAGE',
+	TEXT = 'TEXT'
+}
+
+export enum ArticleType {
+	IT = 'IT',
+	SCIENCE = 'SCIENCE',
+	ECONOMICS = 'ECONOMICS'
+}
+
+export interface ArticleSchema {
+	id: number;
+	title: string;
+	subtitle: string;
+	img: string;
+	views: number;
+	createdAt: string;
+	type: Array<ArticleType>;
+	blocks: Array<ArticleBlock>;
+}
